f07b50de5b92fa33abd956a53d7dad1d755a10fa
Abubakr <dmx0204@mail.com>
2025-09-04 23:01:26 +0500

    Adding URL after deploy

--- backend/routers.py
+++ backend/routers.py
@@ -7,23 +7,44 @@ from .database import get_db
 from typing import Annotated
 from sqlalchemy.orm import Session
 from sqlalchemy import select, delete, update
-from .jwt_Authentication import login_for_access_token, create_access_token, get_password_hash, ACCESS_TOKEN_EXPIRES_MINUTES
+from .jwt_Authentication import (
+    login_for_access_token,
+    create_access_token,
+    get_password_hash,
+    ACCESS_TOKEN_EXPIRES_MINUTES,
+)
 from datetime import datetime, timedelta
 from .jwt_Authentication import get_current_user
 
+
 router = APIRouter(prefix="/resumes", tags=["resumes"])
-user_router = APIRouter(prefix='/users', tags=['users'])
+user_router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/get_list", response_model=list[ResumeInDB])
-def get_list_of_resumes(db: Annotated[Session, Depends(get_db)], current_user:Annotated[User, Depends(get_current_user)]):
+@router.get(
+    "/get_list",
+    response_model=list[ResumeInDB],
+    summary="Получение списка всех резюме авторизованного пользователя",
+)
+def get_list_of_resumes(
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
     user_id = current_user.id
     query = db.execute(select(Resume).where(Resume.user_id == user_id)).scalars().all()
     return query
 
 
-@router.get("/{resume_id}", response_model=ResumeInDB)
-def get_one_resume(resume_id: int, db: Annotated[Session, Depends(get_db)], current_user:Annotated[User, Depends(get_current_user)]):
+@router.get(
+    "/{resume_id}",
+    response_model=ResumeInDB,
+    summary="Получение конкретного резюме по ID",
+)
+def get_one_resume(
+    resume_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
     user_id = current_user.id
     query = select(Resume).where(Resume.id == resume_id, Resume.user_id == user_id)
     resume = db.execute(query).scalar()
@@ -34,11 +55,15 @@ def get_one_resume(resume_id: int, db: Annotated[Session, Depends(get_db)], curr
     return resume
 
 
-@router.post("/", response_model=ResumeInDB)
-def create_resume(new_resume: ResumeCreate, db: Annotated[Session, Depends(get_db)], current_user:Annotated[User, Depends(get_current_user)]):
+@router.post("/", response_model=ResumeInDB, summary="Создание нового резюме")
+def create_resume(
+    new_resume: ResumeCreate,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
     user_id = current_user.id
     resume_data = new_resume.model_dump()
-    resume_data['user_id'] = user_id
+    resume_data["user_id"] = user_id
     resume = Resume(**resume_data)
     db.add(resume)
     db.commit()
@@ -46,22 +71,29 @@ def create_resume(new_resume: ResumeCreate, db: Annotated[Session, Depends(get_d
     return resume
 
 
-@router.patch("/{resume_id}", response_model=ResumeInDB)
+@router.patch(
+    "/{resume_id}",
+    response_model=ResumeInDB,
+    summary="Обновление и улучшение конкретного резюме по ID",
+)
 def update_old_resume(
     resume_id: int,
     resume_to_update: ResumeUpdate,
     db: Annotated[Session, Depends(get_db)],
-    current_user:Annotated[User, Depends(get_current_user)]
+    current_user: Annotated[User, Depends(get_current_user)],
 ):
-    
+
     user_id = current_user.id
     update_data = resume_to_update.model_dump(exclude_unset=True)
-    query = update(Resume).where(Resume.id == resume_id, Resume.user_id == user_id).values(update_data)
+    query = (
+        update(Resume)
+        .where(Resume.id == resume_id, Resume.user_id == user_id)
+        .values(update_data)
+    )
     result = db.execute(query)
     if result.rowcount == 0:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail="Резюме не найдено"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Резюме не найдено"
         )
     db.commit()
     new_query = select(Resume).where(Resume.id == resume_id, Resume.user_id == user_id)
@@ -69,8 +101,11 @@ def update_old_resume(
     return updated_resume
 
 
-@router.delete("/delete_all")
-def delete_all_resumes(db: Annotated[Session, Depends(get_db)], current_user:Annotated[User, Depends(get_current_user)]):
+@router.delete("/delete_all", summary="Удаление всех резюме")
+def delete_all_resumes(
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
     user_id = current_user.id
 
     query = delete(Resume).where(Resume.user_id == user_id)
@@ -79,8 +114,12 @@ def delete_all_resumes(db: Annotated[Session, Depends(get_db)], current_user:Ann
     return {"message": "Все резюме успешно удалены"}
 
 
-@router.delete("/{resume_id}")
-def delete_one_resume(resume_id: int, db: Annotated[Session, Depends(get_db)], current_user:Annotated[User, Depends(get_current_user)]):
+@router.delete("/{resume_id}", summary="Удаление конкретного реюме по ID")
+def delete_one_resume(
+    resume_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
     user_id = current_user.id
     query = delete(Resume).where(Resume.id == resume_id, Resume.user_id == user_id)
     result = db.execute(query)
@@ -93,13 +132,22 @@ def delete_one_resume(resume_id: int, db: Annotated[Session, Depends(get_db)], c
     return {"message": "Резюме успешно удалено"}
 
 
-@router.post('/{resume_id}/improve', response_model=ResumeInDB)
-def improve_resume_by_ai(resume_id:int, resume_input:ResumeInput, db: Annotated[Session, Depends(get_db)], current_user:Annotated[User, Depends(get_current_user)]):
+@router.post(
+    "/{resume_id}/improve",
+    response_model=ResumeInDB,
+    summary="Улучшение резюме через AI",
+)
+def improve_resume_by_ai(
+    resume_id: int,
+    resume_input: ResumeInput,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
     user_id = current_user.id
     query = select(Resume).where(Resume.id == resume_id, Resume.user_id == user_id)
     resume_to_update = db.execute(query).scalar()
     improved_content = resume_input.content
-    improved_content += ' [Improved] ' 
+    improved_content += " [Improved] "
     resume_to_update.content = improved_content
     db.add(resume_to_update)
     db.commit()
@@ -107,20 +155,22 @@ def improve_resume_by_ai(resume_id:int, resume_input:ResumeInput, db: Annotated[
     return resume_to_update
 
 
-
-
-@user_router.post('/registration', response_model=Token)
-def registration(new_user:UserCreate, db: Annotated[Session, Depends(get_db)]):
+@user_router.post(
+    "/registration",
+    response_model=Token,
+    summary="Для регистрации нового пользователя и получения токена",
+)
+def registration(new_user: UserCreate, db: Annotated[Session, Depends(get_db)]):
     query = select(User).where(User.email == new_user.email)
     existing_user = db.execute(query).scalar()
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Пользователь с таким email уже существует')
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Пользователь с таким email уже существует",
+        )
+
     hashed_password = get_password_hash(new_user.password)
-    user = User(
-        email = new_user.email,
-        hash_password = hashed_password
-    )
+    user = User(email=new_user.email, hash_password=hashed_password)
     db.add(user)
     db.commit()
     db.refresh(user)
@@ -131,10 +181,11 @@ def registration(new_user:UserCreate, db: Annotated[Session, Depends(get_db)]):
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@user_router.post("/token", response_model=Token)
+@user_router.post(
+    "/token", response_model=Token, summary="Для авторизации существующего пользователя"
+)
 def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Annotated[Session, Depends(get_db)],
 ):
     return login_for_access_token(form_data=form_data, db=db)
-
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(router)
 app.include_router(user_router)
+origins = [
+    'https://resume-project-z732.onrender.com',
+    'http://localhost:7135'
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
